a4d3b9586f2c163b35ed44d40b7d9f30488f03e2
paulomd2 <paulo.sergio@agenciamd2.com.br>
2015-04-01 11:10:21 -0300

    constantes das tabelas

--- model/constantes.php
+++ model/constantes.php
@@ -9,3 +9,5 @@ require_once 'constantesBanco.php';
 define("TBL_USUARIO",DB_ADMIN.TBL_ADMIN."usuarios ");
 define("TBL_POST",DB_ADMIN.TBL_ADMIN."posts ");
 define("TBL_COMENTARIO",DB_ADMIN.TBL_ADMIN."comentarios ");
+define("TBL_REL_USUARIO_CLIENTE",DB_ADMIN.TBL_ADMIN."clienteUsuario");
+define("TBL_CLIENTE",DB_ADMIN.TBL_ADMIN."clientes ");
