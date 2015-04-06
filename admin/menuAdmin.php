2f661551f59688433fff15b4a1b0f3177b718424
paulomd2 <paulo.sergio@agenciamd2.com.br>
2015-04-06 18:06:01 -0300

    arquivos de nível administrador

---
+++ admin/menuAdmin.php
@@ -0,0 +1,10 @@
+<nav style="float: left; margin-right: 20px ">
+    <ul>
+        <li>
+            <a href="verUsuarios.php">Usuários</a>
+        </li>
+        <li>
+            <a href="verClientes.php">Clientes</a>
+        </li>
+    </ul> 
+</nav>
